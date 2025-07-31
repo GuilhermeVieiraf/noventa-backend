a29614e8e7819b81287f81d82909562b71fb5e0b
GuilhermeVieiraf <guilhermevfelix06@gmail.com>
2025-07-31 19:23:08 -0300

    Initial commit: project structure from Spring Initializr

---
+++ src/test/java/com/noventaorg/NoventaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noventaorg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoventaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
